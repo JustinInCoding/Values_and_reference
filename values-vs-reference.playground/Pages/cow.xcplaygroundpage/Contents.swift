8f208fd0a1ab418161d3e6b9528bf6f5dcaa2c8e
JustinSheng <justin.wang4@icloud.com>
2024-03-31 10:10:40 +0800

    COW

---
+++ values-vs-reference.playground/Pages/cow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,74 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Copyright (c) 2019 Razeware LLC
+// See Copyright Notice page for details about the license.
+
+let owlBear = RWMonster(name: "Owl Bear", hitPoints: 30)
+var enemies: [RWMonster] = [owlBear]
+owlBear.hitPoints += 10
+print(enemies)  // 40, not 30.  Reference semantics :[
+
+final class SwiftReference<T> {
+	var object: T
+	init(_ object: T) {
+		self.object = object
+	}
+}
+
+struct Monster: CustomStringConvertible {
+	private var _monster: SwiftReference<RWMonster>
+	
+	private var _mutatingMonster: RWMonster {
+		mutating get {
+			if !isKnownUniquelyReferenced(&_monster) {
+				print("Making copy")
+				_monster = SwiftReference(_monster.object.copy() as! RWMonster)
+			} else {
+				print("No copy")
+			}
+			return _monster.object
+		}
+	}
+	
+	init(name: String, hitPoints: Int) {
+		_monster = SwiftReference(RWMonster(name: name, hitPoints: hitPoints))
+	}
+	
+	var description: String {
+		return String(describing: _monster.object)
+	}
+	
+	var name: String {
+		get {
+			return _monster.object.name
+		}
+		set {
+			_mutatingMonster.name = newValue
+		}
+	}
+	
+	var hitPoints: Int {
+		get {
+			return _monster.object.hitPoints
+		}
+		set {
+			_mutatingMonster.hitPoints = newValue
+		}
+	}
+}
+
+var troll = Monster(name: "Troll", hitPoints: 30)
+var monsters: [Monster] = [troll]
+troll.hitPoints += 100
+troll.hitPoints += 1000
+troll.hitPoints += 1000
+troll.hitPoints += 1000
+troll.hitPoints += 1000
+troll.hitPoints += 1000
+print(monsters)
+print(troll)
+
+
+//: [Next](@next)
